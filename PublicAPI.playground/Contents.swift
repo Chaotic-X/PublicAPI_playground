335a8fd94be6dfb2e605ee128496b3806f21c8a2
Alex Lundquist <alex@alexlundquist.com>
2020-08-03 11:27:20 -0600

    Model Data complete

--- PublicAPI.playground/Contents.swift
+++ PublicAPI.playground/Contents.swift
@@ -1,3 +1,29 @@
 import UIKit
 
-var str = "Hello, playground"
+//MARK: -Goals
+//GET all categories
+//GET all API's for a specific category
+
+let baseURL = URL(string: "https://api.publicapis.org")
+
+//MARK: -Model file because its out data for our Model
+//Model 1 (potentially?)
+//This is for the "api.publicapis.org/categories"
+struct TopLevelObject {
+  let entries: [String]
+}
+//for the url "api.publicapis.org/entries"
+struct TopLevelObject2 {
+  let count: Int
+  let entries: [Entry] //We want our Array to be an Array of Entry Objects
+}
+
+struct Entry {
+  let API: String
+  let Description: String
+  let Auth: String
+  let HTTPS: Bool
+  let Cors: String
+  let Link: String
+  let Category: String
+}
